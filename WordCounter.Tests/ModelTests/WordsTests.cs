178b68eb9d40b8aeaa4fc84f346fe26d8315d5f5
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-26 13:37:04 -0700

    Add new test for setting Sentence

--- WordCounter.Tests/ModelTests/WordsTests.cs
+++ WordCounter.Tests/ModelTests/WordsTests.cs
@@ -20,5 +20,24 @@ namespace WordCounter.Tests
       string result = newWords.GetSentence();
       Assert.AreEqual(sentence, result);
     }
+    [TestMethod]
+    public void SetSentence_SetSentence_String()
+    {
+      string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend!";
+      Words newWords = new Words(sentence);
+      string updatedSentence = "Puppytopia is the best place to see puppies and find your puppy best friend";
+      newWords.SetSentence(updatedSentence);
+      string result = newWords.GetSentence();
+      Assert.AreEqual(updatedSentence, result);
+    }
+    // [TestMethod]
+    // public void Word_GetWord_String()
+    // {
+    //   string word = "kitty";
+    //   Words newWords = new Words(sentence);
+    //   string result = newWords.GetWord();
+    //   Assert.AreEqual(word, result)
+    //
+    // }
   }
 }
--- WordCounter/Models/Words.cs
+++ WordCounter/Models/Words.cs
@@ -3,13 +3,28 @@ namespace WordCounter.Models
   public class Words
   {
     private string _sentence;
+
     public Words (string sentence)
     {
       _sentence = sentence;
+      // _word = word;
     }
     public string GetSentence()
     {
       return _sentence;
     }
+    public void SetSentence(string newSentence)
+    {
+      _sentence = newSentence;
+    }
+    // public void GetWord()
+    // {
+    //   return _word;
+    // }
+    // public void SetWord()
+    // {
+    //   _word = newWord;
+    // }
+
   }
 }
