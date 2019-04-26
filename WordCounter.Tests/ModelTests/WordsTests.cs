a7698ea3378552c66f9906bd91f2f82f8ebfd2f6
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-26 12:32:28 -0700

    add second test for sentence

--- WordCounter.Tests/ModelTests/WordsTests.cs
+++ WordCounter.Tests/ModelTests/WordsTests.cs
@@ -9,8 +9,16 @@ namespace WordCounter.Tests
     [TestMethod]
     public void WordsConstructor_CreateInstanceOfWords_Words()
     {
-      Words newWords = new Words();
+      Words newWords = new Words("test");
       Assert.AreEqual(typeof(Words), newWords.GetType());
+    }
+    [TestMethod]
+    public void GetSentence_ReturnsSentence_String()
+    {
+      string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend!";
+      Words newWords = new Words(sentence);
+      string result = newWords.GetSentence();
+      Assert.AreEqual(sentence, result);
     }
   }
 }
--- WordCounter/Models/Words.cs
+++ WordCounter/Models/Words.cs
@@ -2,6 +2,14 @@ namespace WordCounter.Models
 {
   public class Words
   {
-
+    private string _sentence;
+    public Words (string sentence)
+    {
+      _sentence = sentence;
+    }
+    public string GetSentence()
+    {
+      return _sentence;
+    }
   }
 }
