0f7143a6b63f0ec0cfb3a3a1573d116f5bc905de
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-27 12:56:20 -0700

    add array method

--- WordCounter.Tests/ModelTests/WordsTests.cs
+++ WordCounter.Tests/ModelTests/WordsTests.cs
@@ -10,23 +10,32 @@ namespace WordCounter.Tests
     [TestMethod]
     public void WordsConstructor_CreateInstanceOfWords_Words()
     {
-      string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend!";
+      string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend";
       string oneWord = "kitty";
       Words newWords = new Words(sentence, oneWord);
       Assert.AreEqual(typeof(Words), newWords.GetType());
     }
-      [TestMethod]
-      public void SetSentenceAndOneWord_SetSentenceAndOneWord_String()
-      {
-        string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend!";
-        string oneWord = "kitty";
-        Words newWords = new Words(sentence, oneWord);
-        string newSentence = "Puppytopia is the best place to see puppies and find your puppy best friend";
-        string newOneWord = "puppy";
-        newWords.SetSentenceAndOneWord(newSentence, newOneWord);
-        string result = newWords.WordsConstructor();
-        Assert.AreEqual(newSentence, newOneWord, result);
-      }
+    [TestMethod]
+    public void TurnToArray_TurnToArray_Array()
+    {
+      string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend";
+      string oneWord = "kitty";
+      string[] result = new string[] {"Kittytopia", "is", "the", "best", "place", "to", "see", "kitties", "and", "find", "your", "kitty", "best", "friend"};
+      Words newWords = new Words(sentence, oneWord);
+      CollectionAssert.AreEqual(result, newWords.TurnToArray());
+    }
+      // [TestMethod]
+      // public void SetSentenceAndOneWord_SetSentenceAndOneWord_String()
+      // {
+      //   string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend!";
+      //   string oneWord = "kitty";
+      //   Words newWords = new Words(sentence, oneWord);
+      //   string newSentence = "Puppytopia is the best place to see puppies and find your puppy best friend";
+      //   string newOneWord = "puppy";
+      //   newWords.SetSentenceAndOneWord(newSentence, newOneWord);
+      //   string result = newWords.WordsConstructor();
+      //   Assert.AreEqual(newSentence, newOneWord, result);
+      // }
     // }
     // [TestMethod]
     // public void GetSentence_ReturnsSentence_String()
--- WordCounter/Models/Words.cs
+++ WordCounter/Models/Words.cs
@@ -6,21 +6,27 @@ namespace WordCounter.Models
   {
     private string _sentence;
     private string _oneWord;
+    private string[] _array;
 
     public Words (string sentence, string oneWord)
     {
       _sentence = sentence;
       _oneWord = oneWord;
+      _array = sentence.Split(' ');
+    }
+    public string[] TurnToArray()
+    {
+      return _array;
     }
     // public string GetSentence()
     // {
     //   return _sentence;
     // }
-    public void SetSentenceAndOneWord(string newSentence, string newOneWord)
-    {
-      _sentence = newSentence;
-      _oneWord = newOneWord;
-    }
+    // public void SetSentenceAndOneWord(string newSentence, string newOneWord)
+    // {
+    //   _sentence = newSentence;
+    //   _oneWord = newOneWord;
+    // }
     // public string GetOneWord()
     // {
     //   return _oneWord;
