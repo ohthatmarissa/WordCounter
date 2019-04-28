98aed44f81d98c759ceb2044a3613df647d728e7
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-27 17:26:50 -0700

    Add counting method

--- WordCounter.Tests/ModelTests/WordsTests.cs
+++ WordCounter.Tests/ModelTests/WordsTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using WordCounter.Models;
 
 namespace WordCounter.Tests
@@ -15,6 +16,16 @@ namespace WordCounter.Tests
       Words newWords = new Words(sentence, oneWord);
       Assert.AreEqual(typeof(Words), newWords.GetType());
     }
+    // [TestMethod]
+    // public void TurnWordsToUpper_TurnWordsToUpper_UpperCaseString()
+    // {
+    //   string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend";
+    //   string oneWord = "kitty";
+    //   string upper = "KITTYTOPIA IS THE BEST PLACE TO SEE KITTIES AND FIND YOUR KITTY BEST FRIEND";
+    //   Words newWords = new Words(sentence, oneWord);
+    //   Assert.AreEqual(upper, newWords.TurnWordsToUpper());
+    // }
+
     [TestMethod]
     public void TurnToArray_TurnToArray_Array()
     {
@@ -23,6 +34,16 @@ namespace WordCounter.Tests
       string[] result = new string[] {"Kittytopia", "is", "the", "best", "place", "to", "see", "kitties", "and", "find", "your", "kitty", "best", "friend"};
       Words newWords = new Words(sentence, oneWord);
       CollectionAssert.AreEqual(result, newWords.TurnToArray());
+    }
+
+    [TestMethod]
+    public void Counting_HowManyTimesWordInSentence_Int()
+    {
+      string sentence = "Kittytopia is the best place to see kitties and find your kitty best friend";
+      string oneWord = "kitty";
+      int result = 1;
+      Words newWords = new Words(sentence, oneWord);
+      Assert.AreEqual(result, newWords.Counting());
     }
       // [TestMethod]
       // public void SetSentenceAndOneWord_SetSentenceAndOneWord_String()
--- WordCounter/Models/Words.cs
+++ WordCounter/Models/Words.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace WordCounter.Models
 {
@@ -8,16 +9,37 @@ namespace WordCounter.Models
     private string _oneWord;
     private string[] _array;
 
+
     public Words (string sentence, string oneWord)
     {
       _sentence = sentence;
       _oneWord = oneWord;
       _array = sentence.Split(' ');
     }
+
+    // public string TurnWordsToUpper()
+    // {
+    //   return upper;
+    // }
+
     public string[] TurnToArray()
     {
       return _array;
     }
+
+    public int Counting()
+    {
+      int count = 0;
+      foreach(string oneWord in _array)
+      {
+        if(oneWord == _oneWord)
+        {
+          count++;
+        }
+      }
+      return count;
+    }
+
     // public string GetSentence()
     // {
     //   return _sentence;
